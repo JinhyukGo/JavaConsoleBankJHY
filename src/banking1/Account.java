effcbab1d86864040ab782b1fb969916881b9034
JinhyukGo <gojinhyukgo@gmail.com>
2024-01-19 11:50:41 +0900

    first commit

--- src/banking1/Account.java
+++ src/banking1/Account.java
@@ -2,8 +2,24 @@ package banking1;
 
 import java.util.Scanner;
 
-public class Account {
+class Bank {
+	String num, name;
+	int balance;
+	
+	public Bank(String num, String name, int balance) {
+		this.num = num;
+		this.name = name;
+		this.balance = balance;
+	}
+	public void showAccInfo() {
+		System.out.println("계좌번호 : " + num);
+		System.out.println("이름 : " + name);
+		System.out.println("잔액 : " + balance);
+	}
+}
 
+public class Account {
+	
 	public static void showMenu() {
 		System.out.println("=== 원하시는 메뉴를 선택하세요. ===");
 		System.out.println("");
@@ -16,82 +32,91 @@ public class Account {
 		System.out.println("============================");
 	}
 
-		public static void main(String[] args) {
-			Scanner scan = new Scanner(System.in);
-			AccountManager handler = new AccountManager();
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		AccountManager handler = new AccountManager(100);
+		
+		while(true) {
+			showMenu();
 			
-			while(true) {
-				showMenu();
-				
-				int choice = scan.nextInt();
-				switch(choice) {
-				case 1:
-					handler.makeAccount();
-					break;					
-				case 2:
-					handler.depositMoney();
-					break;
-				case 3:
-					handler.withdrawMoney();
-					break;
-				case 4:
-					handler.showAccInfo();
-					break;
-				case 5:
-					System.out.println("프로그램 종료");
-					return;
-				}
+			int choice = scan.nextInt();
+			switch(choice) {
+			case 1:
+				handler.makeAccount();
+				break;					
+			case 2:
+				handler.depositMoney();
+				break;
+			case 3:
+				handler.withdrawMoney();
+				break;
+			case 4:
+				handler.showAccInfo();
+				break;
+			case 5:
+				System.out.println("프로그램 종료");
+				return;
 			}
 		}
 	}
 
+	public String accNum;
+
+	public void showAccInfo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
 class AccountManager {
 	
-	private Account[] account;
+	private Bank[] bankAccount;
 	private int accountList;
+	
+	public AccountManager(int num) {
+		bankAccount = new Bank[num];
+		accountList = 0;
+	}
+	
+	String accNum, accName;
+	int accBalance, deposit;
 	
 	public void makeAccount() {
 		Scanner scan = new Scanner(System.in);
-		
-		String accNum, accName;
-		int balance;
-		
 		System.out.println("계좌번호 : "); accNum = scan.nextLine();
 		System.out.println("이름 : "); accName = scan.nextLine();
-		System.out.println("잔액 : "); balance = scan.nextInt();
-		System.out.println("계좌 개설 정보 입력이 완료되었습니다.");
+		System.out.println("잔액 : "); accBalance = scan.nextInt();
+		System.out.println("신규 계좌 개설이 완료되었습니다.");
 	}
 	public void depositMoney() {
-		int deposit;
-		
-		boolean isFind = false;
-		Scanner scan = new Scanner(System.in);
-		System.out.print("계좌번호를 입력하세요 : ");
-		String searchName = scan.nextLine();
-		
-		for(int i = 0 ; i<accountList ; i++) {
-			if(searchName.compareTo(account[i].accNum)==0) {
-				account[i].showAccInfo();
-				System.out.println("** 입력하신 계좌정보를 찾았습니다. **");
-				System.out.println("입금액 : "); deposit = scan.nextInt();
-				System.out.println(" 잔액 : "+(balance+deposit)+"원");
-				isFind = true;
-			}
-		}
-		if(isFind==false) {
-			System.out.println("** 입력하신 계좌정보와 일치하는 계좌가 없습니다. **");
-		} // end of searchInfo
-		
+//		
+//		boolean isFind = false;
+//		Scanner scan = new Scanner(System.in);
+//		System.out.print("계좌번호를 입력하세요 : ");
+//		String searchName = scan.nextLine();
+//		
+//		for(int i = 0 ; i<accountList ; i++) {
+//			if(searchName.compareTo(bankAccount[i].accNum)==0) {
+//				bankAccount[i].showAccInfo();
+//				System.out.println("** 입력하신 계좌정보를 찾았습니다. **");
+//				System.out.println("입금액 : "); deposit = scan.nextInt();
+//				System.out.println(" 잔액 : "+(accBalance+deposit)+"원");
+//				isFind = true;
+//			}
+//		}
+//		if(isFind==false) {
+//			System.out.println("** 입력하신 계좌정보와 일치하는 계좌가 없습니다. **");
+//		} // end of searchInfo
+//		
 	}
 	public void withdrawMoney() {
 		
 	}
 	public void showAccInfo() {
-		
+		for(int i = 0 ; i<accountList ; i++) {
+			bankAccount[i].showAccInfo();
+		}
+		System.out.println("== 전체 정보가 출력되었습니다. ==");
 	}
 }
-
-class Account {
-
-}
-
